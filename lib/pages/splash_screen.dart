d67c260f5f4d79813c57b516554c944cb2dfe7c3
Abdelrahman Rashad <abdelrahman.a.rashad@gmail.com>
2021-05-24 00:44:38 +0200

    create splash screen & put images

---
+++ lib/pages/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:splash_screen_view/SplashScreenView.dart';
+import 'package:world/pages/home.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreenView(
+      navigateRoute: Home(),
+      duration: 3000,
+      imageSize: 200,
+      imageSrc: "image/share.png",
+      backgroundColor: Colors.white,
+    );
+  }
+}
